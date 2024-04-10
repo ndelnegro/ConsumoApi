970556034f88c7a5874074103f649e7c036d75dc
ndelnegro <nicolasdelnegro@gmail.com>
2024-04-10 13:25:31 -0300

    commit

---
+++ ConsumoAPI/ConsumoAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ConsumoAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
